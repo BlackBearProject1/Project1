040909e12f6e69ba40bb6b2ff51a3383f4126633
mpleier <mpleier@gmail.com>
2017-08-10 08:42:29 -0700

    started integrating Google Maps API

--- assets/javascript/choose.js
+++ assets/javascript/choose.js
@@ -92,39 +92,3 @@ function initAutocomplete() {
     map.fitBounds(bounds);
   });
 }
-
-
-var Ireland = "Dublin";
-
-function initialize()
-{
-  geocoder = new google.maps.Geocoder();
-  var latlng = new google.maps.LatLng(53.3496, -6.3263);
-  var mapOptions =
-  {
-    zoom: 8,
-    center: latlng
-  };
-  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-  codeAddress(Ireland);//call the function
-}
-
-function codeAddress(address)
-{
-  geocoder.geocode( {address:address}, function(results, status)
-  {
-    if (status == google.maps.GeocoderStatus.OK)
-    {
-      map.setCenter(results[0].geometry.location);//center the map over the result
-      //place a marker at the location
-      var marker = new google.maps.Marker(
-      {
-          map: map,
-          position: results[0].geometry.location
-      });
-    } else {
-      alert('Geocode was not successful for the following reason: ' + status);
-   }
-  });
-}
-initialize();
