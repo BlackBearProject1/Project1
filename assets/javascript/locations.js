20daf8c298d9486fe8a13971b55ed7879d577781
mpleier <mpleier@gmail.com>
2017-08-11 08:55:26 -0700

    test

--- assets/javascript/locations.js
+++ assets/javascript/locations.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 
     $.ajax({
         url: "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyBjFxuu7fOjkqkLhWGnBRk_W0yUfbPd29k&rankby=distance&type=hiking&location=33.684567,-117.826505&keyword=hiking",
-        dataType: 'JSONP',
+        dataType: 'JSON',
         jsonpCallback: 'callback',
         cache: false,
         type: 'GET',
