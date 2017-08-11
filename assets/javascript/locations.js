81806f1e82838d4c9d766e0fd7652a4cc245b565
mpleier <mpleier@gmail.com>
2017-08-11 08:53:20 -0700

    test

--- assets/javascript/locations.js
+++ assets/javascript/locations.js
@@ -1,1 +1,18 @@
 
+
+$(document).ready(function() {
+
+
+    $.ajax({
+        url: "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyBjFxuu7fOjkqkLhWGnBRk_W0yUfbPd29k&rankby=distance&type=hiking&location=33.684567,-117.826505&keyword=hiking",
+        dataType: 'JSONP',
+        jsonpCallback: 'callback',
+        cache: false,
+        type: 'GET',
+        jsonp: 'callback',
+        success: function (data) {
+            console.log(data);
+        }
+    });
+
+  });
